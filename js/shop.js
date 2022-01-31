998b1f7e20bee908c9a9dab35e3d641d848d5b1d
AgusCC <aguscorrado@gmail.com>
2022-01-31 17:22:17 +0100

    revision 1

--- js/shop.js
+++ js/shop.js
@@ -67,6 +67,8 @@ var total = 0;
 function buy(id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
+   
+
     var products  = document.querySelectorAll('.products')
     
     for( var i = 0; i <products.length; i+1) {
